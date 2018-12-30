5b6bd961d7863f83ed4f4b21c5515a89ad775221
Raven65 <286506286@qq.com>
2018-12-30 15:47:06 +0800

    update

--- fighter.py
+++ fighter.py
@@ -49,6 +49,8 @@ class Fighter():
 				return "attack"
 		if mode == "minimax":
 			return self.minimax(self, enemy, 0, 4, -30, 30)[1];
+		if mode == "minimax_random":
+			return self.minimax_random_eva(self, enemy, 0, 4, -30, 30)[1];
 		if mode == "user":
 			# 询问用户键盘输入操作指令
 			act_intro = "(" + "".join([" %s:%s," % (str(value), key) for key, value in self.actions.items() if
@@ -109,3 +111,60 @@ class Fighter():
 				del temp_a
 				del temp_b
 			return [beta, best_move]
+
+	def minimax_random_eva(self, a, b, player, round, alpha, beta):
+		if round < 0:
+			return [self.eva(a, b), ""]
+		if a.hp <= 0 or b.hp <= 0:
+			return [self.eva(a, b), ""]
+		if player == 0:
+			move = ["attack", "defend"]
+			if a.mp >= 4:
+				move.append("counter attack")
+			if a.mp >= 6:
+				move.append("super attack")
+			best_move = move[0]
+			for x in move:
+				temp_a = copy.deepcopy(a)
+				temp_b = copy.deepcopy(b)
+				fn.fight_function[x](temp_a, temp_b)
+				res = self.minimax(temp_a, temp_b, 1, round - 1, alpha, beta)
+				if res[0] > alpha:
+					alpha = res[0]
+					best_move = x
+				if alpha >= beta:
+					return [alpha, best_move]
+				del temp_a
+				del temp_b
+			return [alpha, best_move]
+		elif player == 1:
+			move = ["attack", "defend"]
+			if b.mp >= 4:
+				move.append("counter attack")
+			if b.mp >= 6:
+				move.append("super attack")
+			best_move = move[0]
+			for x in move:
+				temp_a = copy.deepcopy(a)
+				temp_b = copy.deepcopy(b)
+				fn.fight_function[x](temp_b, temp_a)
+				res = self.minimax(temp_a, temp_b, 0, round - 1, alpha, beta)
+				if res[0] < beta:
+					beta = res[0]
+					best_move = x
+				if alpha >= beta:
+					return [beta, best_move]
+				del temp_a
+				del temp_b
+			return [beta, best_move]
+
+	def eva(self, a, b):
+		p = random.randint(1, 4)
+		if p == 1:
+			return a.hp - b.hp
+		elif p == 2:
+			return a.hp + a.shield - b.hp - b.shield
+		elif p == 3:
+			return a.hp + a.mp - b.hp - b.mp
+		elif p == 4:
+			return a.hp + a.shield + a.mp - b.hp - b.mp - b.shield
